2d0d0e57112958f69dc3c7cbb8ec7e0e195274c2
personal <suhaillahmadd0@gmail.com>
2025-03-05 05:37:11 +0530

    - added examples in controller

--- src/controllers/v1/[file-name].controller.ts
+++ src/controllers/v1/[file-name].controller.ts
@@ -1,6 +1,16 @@
 // this file contains the controller for the [file-name] domain
 
-import { Controller, Get } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from "@nestjs/common";
+// import { FileNameService } from "../../services/[file-name].service";
+// import { FileNameEntity } from "../../domain/entities/[file-name].entity";
 
 @Controller("v1/[file-name]")
 export class FileNameController {
@@ -9,5 +19,21 @@ export class FileNameController {
   // @Get()
   // findAll(): Promise<FileNameEntity[]> {
   //   return this.fileNameService.findAll();
+  // }
+  // @Get(":id")
+  // findOne(@Param("id") id: string): Promise<FileNameEntity | null> {
+  //   return this.fileNameService.findOne(id);
+  // }
+  // @Post()
+  // create(@Body() fileNameEntity: FileNameEntity): Promise<FileNameEntity> {
+  //   return this.fileNameService.create(fileNameEntity);
+  // }
+  // @Put(":id")
+  // update(@Param("id") id: string, @Body() fileNameEntity: FileNameEntity): Promise<FileNameEntity> {
+  //   return this.fileNameService.update(id, fileNameEntity);
+  // }
+  // @Delete(":id")
+  // delete(@Param("id") id: string): Promise<void> {
+  //   return this.fileNameService.delete(id);
   // }
 }
